7e5f47638721de56d0ca3c142b793a2be72ee0d7
DevEchoX2 <DevEchoX@gmail.com>
2025-11-21 09:04:12 -0500

    Update apps.js

--- apps.js
+++ apps.js
@@ -2,7 +2,7 @@
 
 const apps = [
   { title: "Discord", image: "images/discord.jpg", url: "https://discord.com" },
-  { title: "Helios", image: "images/Helios.gpg", url: "https://open.spotify.com" },
+  { title: "Helios", image: "images/Helios.jpg", url: "https://devechox2.github.io/ArrowUBG/apps/Helios-Offline.html" },
   { title: "AI", image: "images/AI.jpg", url: "https://devechox2.github.io/ArrowUBG/apps/AI.html" },
   { title: "Twitch", image: "images/twitch.jpg", url: "https://twitch.tv" }
 ];
